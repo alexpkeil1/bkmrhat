5abc8a77c4233be1d07693a82c4383baef45331c
Alex Keil <alexpkeil1@users.noreply.github.com>
2020-09-13 00:41:12 -0400

    Create test_pred.R

---
+++ tests/test_pred.R
@@ -0,0 +1,17 @@
+cat("Testing parallel")
+library(bkmrhat)
+set.seed(111)
+dat <- bkmr::SimData(n = 100, M = 4)
+expit <- function(mu) 1/(1+exp(-mu))
+y <- rbinom(100, 1, expit(dat$y))
+Z <- dat$Z
+X <- dat$X
+set.seed(111)
+Sys.setenv(R_FUTURE_SUPPORTSMULTICORE_UNSTABLE="quiet")
+future::plan(strategy = future::multiprocess)
+fitkm.list <- kmbayes_parallel(nchains=2, y = y, Z = Z, X = X, iter = 10,
+                               verbose = FALSE, varsel = TRUE, family="binomial")
+sinkit = kmbayes_diag(fitkm.list)
+sinkit = suppressWarnings(predict(comb_bkmrfits(fitkm.list)))
+
+closeAllConnections()
