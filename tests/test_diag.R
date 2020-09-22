d7331c0f58b4f209ad244243d9ceb0fc72dc2c3a
Alex Keil <alex.keil@gmail.com>
2020-09-22 16:53:18 -0400

    more tests

---
+++ tests/test_diag.R
@@ -0,0 +1,18 @@
+cat("Testing diag")
+library(bkmrhat)
+set.seed(111)
+dat <- bkmr::SimData(n = 50, M = 4)
+y <- dat$y
+Z <- dat$Z
+X <- dat$X
+set.seed(111)
+Sys.setenv(R_FUTURE_SUPPORTSMULTICORE_UNSTABLE="quiet")
+future::plan(strategy = future::multiprocess)
+fitkm.list <- kmbayes_parallel(nchains=2, y = y, Z = Z, X = X, iter = 10,
+                               verbose = FALSE, varsel = TRUE)
+
+kmbayes_diagnose(fitkm.list)
+combobj = comb_bkmrfits(fitkm.list)
+stopifnot(inherits(combobj, "bkmrfit"))
+kmbayes_diagnose(combobj)
+closeAllConnections()
---
+++ tests/test_invisibles.R
@@ -0,0 +1,36 @@
+#### testing invisible functions ####
+library(bkmrhat)
+
+set.seed(111)
+dat <- bkmr::SimData(n = 50, M = 5, ind=1:3, Zgen="realistic")
+y <- dat$y
+Z <- dat$Z
+X <- cbind(dat$X, rnorm(50))
+
+# run 10 initial iterations for a model with only 2 exposures
+Z2 = Z[,1:2]
+kmfitbma.start <- suppressWarnings(bkmr::kmbayes(y = y, Z = Z2, X = X, iter = 10, verbose = FALSE, varsel = TRUE, est.h = TRUE))
+
+
+
+#.extractparms
+suppressWarnings(bkmrhat:::.extractparms(kmfitbma.start))
+
+
+
+# .diag_par 
+suppressWarnings(bkmrhat:::.diag_par(list(kmfitbma.start, kmfitbma.start)))
+
+
+
+
+# .diag
+suppressWarnings(bkmrhat:::.diag(kmfitbma.start))
+
+
+
+
+# .predictivemean
+
+suppressWarnings( bkmrhat:::.predictivemean(kmfitbma.start))
+suppressWarnings(bkmrhat:::.predictivemean(kmfitbma.start, ptype='sd.fit'))
